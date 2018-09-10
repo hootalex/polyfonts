14ebf39b3ce222a23aabb82f4c0a3abde47e170e
Glitch (skyhavenfonts) <none>
2018-09-10 15:21:06 +0000

    💉🏦 Updated with Glitch

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-/* If you're feeling fancy you can add interactivity
+/* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// script.js
-
-//define background classes in css
-var backgroundClasses = ['fps1', 'fps2', 'fps3', 'fps4', 'fps5'];
-
+// script.js 
+ 
+//define background classes in css 
+var backgroundClasses = ['fps1', 'fps2', 'fps3', 'fps4', 'fps5']; 
+ 
 (function changeBackground() {
 	var nOfBackgrounds = backgroundClasses.length;
-	var i=0;
-
+	var i=0; 
+	
 	setInterval(function() {
   	console.log("value of i=" + i);
     console.log("class removed: " + backgroundClasses[i]);
@@ -18,45 +18,45 @@ var backgroundClasses = ['fps1', 'fps2', 'fps3', 'fps4', 'fps5'];
 		$('.intro, .top, .copy, .rus').addClass(backgroundClasses[newClass]);
 		i = newClass;
     console.log("class added: " + backgroundClasses[i]);
-	}, 100);
-
+	}, 100);	
+ 
 })();
 
 $(function() {
-  $(".theme").click(function() {
+  $(".theme").click(function() {  
     $('body').toggleClass("dark");
-    $('p').toggleClass("darkline");
+    $('p').toggleClass("darkline");  
     $('.theme').toggleClass("light");
   $('body').removeClass("red yellow blue green purple");
   });
 });
 
 $(function() {
-  $(".red").click(function() {
+  $(".red").click(function() {  
     $('body').attr('class', 'red');
   });
 });
 
 $(function() {
-  $(".yellow").click(function() {
+  $(".yellow").click(function() {  
     $('body').attr('class', 'yellow');
   });
 });
 
 $(function() {
-  $(".green").click(function() {
+  $(".green").click(function() {  
     $('body').attr('class', 'green');
   });
 });
 
 $(function() {
-  $(".blue").click(function() {
+  $(".blue").click(function() {  
     $('body').attr('class', 'blue');
   });
 });
 
 $(function() {
-  $(".purple").click(function() {
+  $(".purple").click(function() {  
     $('body').attr('class', 'purple');
   });
 });
