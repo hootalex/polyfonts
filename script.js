03cbb65187d1c5110410e88505eb27809071d212
Alex Tomlinson <alexdangersky@gmail.com>
2018-09-25 15:26:08 -0400

    Paywhatyouwant

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ var backgroundClasses = ['fps1', 'fps2', 'fps3', 'fps4', 'fps5'];
 	var i=0;
 
 	setInterval(function() {
-  	console.log("value of i=" + i);
-    console.log("class removed: " + backgroundClasses[i]);
+  	// console.log("value of i=" + i);
+    // console.log("class removed: " + backgroundClasses[i]);
 		$('.intro, .top, .copy, .rus').removeClass(backgroundClasses[i]);
 		var newClass = (i+1) % nOfBackgrounds;
 		$('.intro, .top, .copy, .rus').addClass(backgroundClasses[newClass]);
 		i = newClass;
-    console.log("class added: " + backgroundClasses[i]);
+    // console.log("class added: " + backgroundClasses[i]);
 	}, 100);
 
 })();
@@ -29,13 +29,13 @@ var backgroundClasses2 = ['casual1', 'casual2', 'casual3', 'casual4', 'casual5']
 	var i=0;
 
 	setInterval(function() {
-  	console.log("value of i=" + i);
-    console.log("class removed: " + backgroundClasses2[i]);
+  	// console.log("value of i=" + i);
+    // console.log("class removed: " + backgroundClasses2[i]);
 		$('.casual').removeClass(backgroundClasses2[i]);
 		var newClass = (i+1) % nOfBackgrounds;
 		$('.casual').addClass(backgroundClasses2[newClass]);
 		i = newClass;
-    console.log("class added: " + backgroundClasses2[i]);
+    // console.log("class added: " + backgroundClasses2[i]);
 	}, 100);
 
 })();
@@ -83,4 +83,26 @@ $(document).ready(function(){
     $(".moonrisealt").keyup(function(){
         $(".moonrisefill").html($(this).html());
     });
+});
+
+$(document).ready(function() {
+    $('#paypalamt').keyup(function() {
+      var amount = $('#paypalamt').val();
+			var url = "https://www.paypal.com/cgi-bin/webscr?business=hoot@birdlover.com&cmd=_xclick&currency_code=USD&amount=";
+			var product = "&item_name=Polyphony%20Fonts";
+			$("#payment").attr("href", url+amount+product);
+			console.log(url+amount+product);
+    });
+});
+
+$(function() {
+  $(".download").click(function() {
+    $('.soliciting').addClass('show');
+  });
+});
+
+$(function() {
+  $("#close").click(function() {
+    $('.soliciting').removeClass('show');
+  });
 });
